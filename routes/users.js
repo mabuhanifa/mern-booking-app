4296929f4e905430d9b2681645d5483ad0caac74
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-07 01:46:49 +0600

    verifyToken added

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,16 @@ import {
     getUsers,
     updateUser
 } from "../controllers/userController.js";
+import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("hello user, you are logged in and you can delete your account");
+});
 /*
 ----------User Routes api------------
 ----------Includes CRUD operations------------
