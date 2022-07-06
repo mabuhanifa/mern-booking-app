785d44ca60d5d0908f3619f802e8847951921984
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-07 03:09:19 +0600

    roomsroutes and rommControllers updated

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,36 @@
 import express from "express";
+import {
+    createRoom,
+    deleteRoom,
+    getRoom,
+    getRooms,
+    updateRoom
+} from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
+/*
+----------Creating RoomsRoutes api------------
+----------Includes CRUD operations------------
+*/
 
+//Create a Room
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//Update a Room
+
+router.put("/:id", verifyAdmin, updateRoom);
+
+// Delete a Room
+
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+// Get a Room by id
+
+router.get("/:id", getRoom);
+
+// Get all Rooms
+
+router.get("/", getRooms);
 
 export default router;
