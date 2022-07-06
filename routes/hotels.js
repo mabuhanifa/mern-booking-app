340ec609a6d743ec64c6ce1d8b813c872846f340
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-06 23:15:11 +0600

    crud routes added

--- routes/hotels.js
+++ routes/hotels.js
@@ -3,9 +3,11 @@ import Hotel from "../models/Hotel.js";
 const router = express.Router();
 
 /*
-Create a new hotelRoutes api
+----------Creating HotelRoutes api------------
+----------Includes CRUD operations------------
 */
 
+//Create Route
 router.post("/", async (req, res) => {
   const newHotel = new Hotel(req.body);
 
@@ -17,4 +19,51 @@ router.post("/", async (req, res) => {
   }
 });
 
+//Update Route
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete Route
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get Route
+
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get all Route
+
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 export default router;
