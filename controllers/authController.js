fb10bf3dcdb9250bbaf4f8b1fbb5583e8d5f4a0a
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-07 01:12:56 +0600

    cookie-parser added

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,8 +31,13 @@ export const login = async (req, res, next) => {
     );
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username!"));
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).send({...otherDetails});
+    res.status(200).send({ ...otherDetails });
   } catch (err) {
     next(err);
   }
