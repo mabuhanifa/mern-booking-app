310808dd87fee049912260cb88ab8a3f431ca513
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-23 22:44:25 +0600

    userModel updated

--- models/User.js
+++ models/User.js
@@ -11,21 +11,24 @@ const UserSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    password: {
-      type: String,
-      required: true,
-    },
     country: {
       type: String,
+      required: true,
     },
     img: {
       type: String,
     },
     city: {
       type: String,
+      required: true,
     },
     phone: {
       type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
     },
     isAdmin: {
       type: Boolean,
